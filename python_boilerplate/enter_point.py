c8b8171cf6d3565625b783d32ed39ef7c497b8c3
9tdc <tianen.gao05@gmail.com>
2026-04-27 02:11:44 -0700

    Update enter_point.py

--- python_boilerplate/enter_point.py
+++ python_boilerplate/enter_point.py
@@ -1,1 +1,9 @@
 
+# main.py
+
+def main():
+    print("Environment is set up. Ready to code!")
+
+# Fill in the blanks to check if this script is being run directly
+if __name__ == "__main__":
+    main()
